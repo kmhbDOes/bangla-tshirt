269fd4cdb7bfdc8f1c3c515408cb6dcaafde5072
kmhbDOes <kmhbipul@gmail.com>
2023-04-06 21:35:40 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Home = () => {
+  const tshirts = useLoaderData();
+  console.log(tshirts);
+  return <div>This is tshirts:{tshirts.length}</div>;
+};
+
+export default Home;
